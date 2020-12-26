4be618a9d59b2f3d585b3b664b2bf6f9d474244b
Brion Vibber <brion@pobox.com>
2020-12-26 10:36:41 -0800

    Update to use periodic data instead of polling
    Instead of subscribing to 1sec timer and requesting the data
    every time, ask for a subscription to the data on a 1-second
    periodic refresh. This is more efficient, and can be changed
    to per-frame increment if needed later.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -49,6 +49,7 @@ namespace WikiWindowSeat
     public partial class MainWindow : Window
     {
         private SimConnect Sim;
+        private Object Marker;
 
         private const uint WIN32_SIMCONNECT_EVENT = 0x0402;
 
@@ -69,6 +70,9 @@ namespace WikiWindowSeat
             {
                 if (Sim != null)
                 {
+                    // This processes any incoming messages via SimConnect
+                    // and dispatches to my callback delegates.
+                    // In C++ you'd call SimConnect_Dispatch() in a loop here.
                     Sim.ReceiveMessage();
                 }
                 handled = true;
@@ -86,22 +90,23 @@ namespace WikiWindowSeat
             {
                 StatusLabel.Content = "Connecting...";
 
+                // The connection needs our Win32 window handle to route messages...
                 IntPtr hWnd = new WindowInteropHelper(this).EnsureHandle();
                 Sim = new SimConnect("WikiWindowSeat", hWnd, WIN32_SIMCONNECT_EVENT, null, 0);
                 StatusLabel.Content = "Connected!";
 
+                // Set up a data record with the user properties we want to fetch
                 Sim.AddToDataDefinition(DEFINE_ID.FETCH_LOC, "Plane Altitude", "feet", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, 0);
                 Sim.AddToDataDefinition(DEFINE_ID.FETCH_LOC, "Plane Latitude", "degrees", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, 8);
                 Sim.AddToDataDefinition(DEFINE_ID.FETCH_LOC, "Plane Longitude", "degrees", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, 16);
                 Sim.RegisterDataDefineStruct<FetchLoc>(DEFINE_ID.FETCH_LOC);
 
-                Sim.OnRecvEvent += this.Sim_RecvEvent;
-                Sim.OnRecvSimobjectDataBytype += this.Sim_RecvSimobjectDataBytype;
+                // Request user position, to be updated every second until we cancel it.
+                Sim.OnRecvSimobjectData += this.Sim_RecvSimobjectData;
+                Sim.RequestDataOnSimObject(REQUEST_ID.FETCH_LOC, DEFINE_ID.FETCH_LOC, SimConnect.SIMCONNECT_OBJECT_ID_USER, SIMCONNECT_PERIOD.SECOND, 0, 0, 0, 0);
 
-                // SHIT YEAH IT WORKS
-                Sim.Text(SIMCONNECT_TEXT_TYPE.PRINT_WHITE, 1.0f, SIMCONNECT_EVENT_FLAG.DEFAULT, "welcome to fun town");
-
-                Sim.SubscribeToSystemEvent(EVENT_ID.TIMER_1HZ, "1sec");
+                // Also make a visible connection
+                Sim.Text(SIMCONNECT_TEXT_TYPE.PRINT_WHITE, 1.0f, SIMCONNECT_EVENT_FLAG.DEFAULT, "Test app connected");
             }
         }
 
@@ -116,20 +121,12 @@ namespace WikiWindowSeat
             }
         }
 
-        private void Sim_RecvEvent(SimConnect sender, SIMCONNECT_RECV_EVENT data)
-        {
-            if (data.uEventID == (uint)EVENT_ID.TIMER_1HZ)
-            {
-                // Ask for data about the user
-                Sim.RequestDataOnSimObjectType(REQUEST_ID.FETCH_LOC, DEFINE_ID.FETCH_LOC, 0, SIMCONNECT_SIMOBJECT_TYPE.USER);
-                StatusLabel.Content = "Requesting data...";
-            }
-        }
-
-        private void Sim_RecvSimobjectDataBytype(SimConnect sender, SIMCONNECT_RECV_SIMOBJECT_DATA_BYTYPE data)
+        private void Sim_RecvSimobjectData(SimConnect sender, SIMCONNECT_RECV_SIMOBJECT_DATA data)
         {
             if (data.dwRequestID == (uint)REQUEST_ID.FETCH_LOC)
             {
+                // Do something with the user position.
+                // For now just display it in the app UI.
                 FetchLoc loc = (FetchLoc)data.dwData[0];
                 StatusLabel.Content = String.Format("Got data: {0} {1} {2}", loc.alt, loc.lat, loc.lon);
             }
---
+++ readme.md
@@ -0,0 +1,8 @@
+WikiWindowSeat
+==============
+
+This is currently a bare skeleton C# SimConnect project, which connects to Microsoft Flight Simulator and reports back the user's current position every second in altitude and lat/lon.
+
+Over the coming days I plan to fetch data on points of interest from Wikipedia via web services and show a map and article extracts.
+
+But for now, it's just a start on getting a working SimConnect app. :)
